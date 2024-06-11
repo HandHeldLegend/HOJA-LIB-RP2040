58f0b4afaf8eb41573a84eeb5c4921ae5ceb99e3
Mitch Cairns <mitch.cairns@handheldlegend.com>
2024-06-10 21:18:02 -0700

    Add CPP guards for interval

--- include/utilities/interval.h
+++ include/utilities/interval.h
@@ -5,7 +5,15 @@
 #include <stdbool.h>
 #include "hoja_types.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool interval_run(uint32_t timestamp, uint32_t interval, interval_s *state);
 bool interval_resettable_run(uint32_t timestamp, uint32_t interval, bool reset, interval_s *state);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
