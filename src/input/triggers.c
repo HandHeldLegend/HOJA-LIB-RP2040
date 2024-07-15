74c49407754cb714a5398f8ce1775c9f766a24df
Mitch Cairns <mitch.cairns@handheldlegend.com>
2024-07-14 20:33:52 -0700

    Adjust trigger calibration with deadzone

--- src/input/triggers.c
+++ src/input/triggers.c
@@ -3,7 +3,7 @@
 float _lt_scaler = 1;
 float _rt_scaler = 1;
 bool _trigger_calibration = false;
-#define TRIGGER_DEADZONE 250
+#define TRIGGER_DEADZONE 100
 
 void triggers_set_disabled(bool left_right, bool disabled)
 {
@@ -17,8 +17,8 @@ void triggers_set_disabled(bool left_right, bool disabled)
 void triggers_start_calibration()
 {
     rgb_flash(COLOR_ORANGE.color, -1);
-    global_loaded_settings.trigger_l.lower = 0x7FF;
-    global_loaded_settings.trigger_r.lower = 0x7FF;
+    global_loaded_settings.trigger_l.lower = 0x7FFF;
+    global_loaded_settings.trigger_r.lower = 0x7FFF;
     global_loaded_settings.trigger_l.upper = 0;
     global_loaded_settings.trigger_r.upper = 0;
     _trigger_calibration = true;
@@ -32,13 +32,10 @@ void triggers_stop_calibration()
 
 void triggers_scale(int left_trigger_in, int *left_trigger_out, int right_trigger_in, int *right_trigger_out)
 {
-    
-
     if(_trigger_calibration)
     {
-
-        if( (left_trigger_in+TRIGGER_DEADZONE)  < (int)global_loaded_settings.trigger_l.lower) global_loaded_settings.trigger_l.lower = (uint16_t) left_trigger_in+TRIGGER_DEADZONE;
-        if( (right_trigger_in+TRIGGER_DEADZONE) < (int)global_loaded_settings.trigger_r.lower) global_loaded_settings.trigger_r.lower = (uint16_t) right_trigger_in+TRIGGER_DEADZONE;
+        if( (left_trigger_in)  < (int)global_loaded_settings.trigger_l.lower) global_loaded_settings.trigger_l.lower = (uint16_t) left_trigger_in   & 0x7FFF;
+        if( (right_trigger_in) < (int)global_loaded_settings.trigger_r.lower) global_loaded_settings.trigger_r.lower = (uint16_t) right_trigger_in  & 0x7FFF;
 
         if((uint16_t) left_trigger_in  > global_loaded_settings.trigger_l.upper) global_loaded_settings.trigger_l.upper = (uint16_t) left_trigger_in;
         if((uint16_t) right_trigger_in > global_loaded_settings.trigger_r.upper) global_loaded_settings.trigger_r.upper = (uint16_t) right_trigger_in;
@@ -91,7 +88,7 @@ void triggers_scale_init()
     if(global_loaded_settings.trigger_l.upper>0)
     {
         // First calculate our full range
-        range = global_loaded_settings.trigger_l.upper - (global_loaded_settings.trigger_l.lower);
+        range = global_loaded_settings.trigger_l.upper - (global_loaded_settings.trigger_l.lower + TRIGGER_DEADZONE);
 
         // We need to ensure our scale results in a full range of 0xFFF
         _lt_scaler = (float) 0xFFF / (float) range;
@@ -100,7 +97,7 @@ void triggers_scale_init()
     if(global_loaded_settings.trigger_r.upper>0)
     {
         // First calculate our full range
-        range = global_loaded_settings.trigger_r.upper - (global_loaded_settings.trigger_r.lower);
+        range = global_loaded_settings.trigger_r.upper - (global_loaded_settings.trigger_r.lower + TRIGGER_DEADZONE);
 
         // We need to ensure our scale results in a full range of 0xFFF
         _rt_scaler = (float) 0xFFF / (float) range;
