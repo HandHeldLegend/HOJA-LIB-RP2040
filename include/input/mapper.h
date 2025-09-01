b56aedfc4a458dcf711088417b5a85ea77f8915e
Mitch Cairns <mitchellcairns@gmail.com>
2025-09-01 13:04:55 -0700

    Remap initial changes

--- include/input/mapper.h
+++ include/input/mapper.h
@@ -3,6 +3,11 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+typedef void (*mapper_check_cb)(void);
+
+typedef void (*mapper_update_button_cb)(bool down);
+typedef void (*mapper_update_joystick_cb)(int16_t);
+typedef void (*mapper_update_trigger_cb)(uint16_t);
 
 // The mapper is a system that allows for a very broad range of mappings for input and output.
 // You can map digital inputs to analog outputs, and map analog inputs to digital outputs. 
@@ -33,32 +38,40 @@ typedef enum
     MAPPER_CODE_RG_UPPER, // Grip button right upper
     MAPPER_CODE_LG_LOWER, // Grip button left lower
     MAPPER_CODE_RG_LOWER, // Grip button right lower
-    MAPPER_CODE_LX_UP,
-    MAPPER_CODE_LX_DOWN,
+    MAPPER_CODE_LT_ANALOG,
+    MAPPER_CODE_RT_ANALOG,
     MAPPER_CODE_LX_RIGHT,
     MAPPER_CODE_LX_LEFT,
-    MAPPER_CODE_RX_UP,
-    MAPPER_CODE_RX_DOWN,
-    MAPPER_CODE_RY_RIGHT,
+    MAPPER_CODE_LY_UP,
+    MAPPER_CODE_LY_DOWN,
+    MAPPER_CODE_RX_RIGHT,
+    MAPPER_CODE_RX_LEFT,
+    MAPPER_CODE_RY_UP,
     MAPPER_CODE_RY_DOWN,
-    MAPPER_CODE_LT_ANALOG,
-    MAPPER_CODE_RT_ANALOG
+    MAPPER_CODE_MAX,
 } mapper_code_t;
+
+#define MAPPER_CODE_IDX_TRIGGER_START  MAPPER_CODE_LT_ANALOG
+#define MAPPER_CODE_IDX_JOYSTICK_START MAPPER_CODE_LX_RIGHT
+
+typedef struct 
+{
+    int8_t map[36]; // 32 used, but we have a reserved
+} mapper_profile_s;
+
+#define MAPPER_PROFILE_SIZE sizeof(mapper_profile_s)
 
 typedef struct 
 {
     uint32_t digital_inputs;
-    int16_t lx;
-    int16_t ly;
-    int16_t rx;
-    int16_t ry;
-    uint16_t lt;
-    uint16_t rt;
+    uint16_t joysticks_raw[8];
+    int16_t joysticks_combined[4];
+    uint16_t triggers[2];
 } mapper_input_s;
 
 typedef struct 
 {
-    int16_t lx_storage;
+    int16_t lx_threshold;
     int16_t ly_threshold;
     int16_t rx_threshold;
     int16_t ry_threshold;
@@ -68,162 +81,6 @@ typedef struct
 
 #define MAPPER_PROFILE_GLOBAL_ANALOG_SIZE sizeof(mapper_global_analog_s)
 
-typedef struct 
-{
-    int8_t a_btn;
-    int8_t b_btn;
-
-    int8_t d_up;
-    int8_t d_down;
-    int8_t d_left;
-    int8_t d_right;
-
-    int8_t c_up;
-    int8_t c_down;
-    int8_t c_left;
-    int8_t c_right;
-
-    int8_t z_btn;
-    int8_t l_btn;
-    int8_t r_btn;
-    int8_t start_btn;
-} mapper_profile_n64_s;
-
-#define MAPPER_PROFILE_N64_SIZE sizeof(mapper_profile_n64_s)
-
-typedef struct 
-{
-    int8_t a_btn;
-    int8_t b_btn;
-    int8_t x_btn;
-    int8_t y_btn;
-
-    int8_t d_up;
-    int8_t d_down;
-    int8_t d_left;
-    int8_t d_right;
-
-    int8_t start_btn;
-    int8_t select_btn;
-    int8_t l_btn;
-    int8_t r_btn;
-} mapper_profile_snes_s;
-
-#define MAPPER_PROFILE_SNES_SIZE sizeof(mapper_profile_snes_s)
-
-typedef struct 
-{
-    int8_t a_btn;
-    int8_t b_btn;
-    int8_t x_btn;
-    int8_t y_btn;
-
-    int8_t d_up;
-    int8_t d_down;
-    int8_t d_left;
-    int8_t d_right;
-
-    int8_t z_btn;
-    int8_t l_btn;
-    int8_t r_btn;
-    int8_t start_btn;
-
-    int8_t sx_up;
-    int8_t sx_down;
-    int8_t sy_right;
-    int8_t sy_left;
-
-    int8_t cx_up;
-    int8_t cx_down;
-    int8_t cy_right;
-    int8_t cy_left;
-    
-    int8_t l_analog;
-    int8_t r_analog;
-
-    int8_t l_analog_static;
-    int8_t r_analog_static;
-} mapper_profile_gamecube_s;
-
-#define MAPPER_PROFILE_N64_SIZE sizeof(mapper_profile_n64_s)
-
-typedef struct 
-{
-    int8_t a_btn;
-    int8_t b_btn;
-    int8_t x_btn;
-    int8_t y_btn;
-
-    int8_t d_up;
-    int8_t d_down;
-    int8_t d_left;
-    int8_t d_right;
-
-    int8_t lb_btn;
-    int8_t rb_btn;
-    int8_t sl_btn;
-    int8_t sr_btn;
-
-    int8_t guide_btn;
-    int8_t menu_btn;
-    int8_t back_btn;
-    int8_t reserved;
-
-    int8_t lx_up;
-    int8_t lx_down;
-    int8_t ly_right;
-    int8_t ly_left;
-
-    int8_t rx_up;
-    int8_t rx_down;
-    int8_t ry_right;
-    int8_t ry_left;
-    
-    int8_t l_analog;
-    int8_t r_analog;
-
-    int8_t l_analog_static;
-    int8_t r_analog_static;
-} mapper_profile_xinput_s;
-
-#define MAPPER_PROFILE_XINPUT_SIZE sizeof(mapper_profile_xinput_s)
-
-typedef struct 
-{
-    int8_t a_btn;
-    int8_t b_btn;
-    int8_t x_btn;
-    int8_t y_btn;
-
-    int8_t d_up;
-    int8_t d_down;
-    int8_t d_left;
-    int8_t d_right;
-
-    int8_t l_btn;
-    int8_t r_btn;
-    int8_t zl_btn;
-    int8_t zr_btn;
-
-    int8_t sl_btn;
-    int8_t sr_btn;
-
-    int8_t plus_btn;
-    int8_t minus_btn;
-    int8_t home_btn;
-    int8_t capture_btn;
-
-    int8_t lx_up;
-    int8_t lx_down;
-    int8_t ly_right;
-    int8_t ly_left;
-
-    int8_t rx_up;
-    int8_t rx_down;
-    int8_t ry_right;
-    int8_t ry_left;
-} mapper_profile_switch_s;
-
-#define MAPPER_PROFILE_SWITCH_SIZE sizeof(mapper_profile_switch_s)
+void mapper_process();
 
 #endif
--- src/input/mapper.c
+++ src/input/mapper.c
@@ -1,1 +1,225 @@
 #include "input/mapper.h"
+#include "input_shared_types.h"
+#include <stdlib.h>
+
+
+typedef void (*mode_mapper_cb)(input_data_s *input);
+
+
+mode_mapper_cb _mode_mapper_cb;
+mapper_profile_s *_mapper_profile;
+
+mapper_update_button_cb _mapper_button_updaters[MAPPER_CODE_MAX];
+mapper_input_s _mapper_input = {0};
+mapper_input_s _mapper_output = {0};
+
+void _mode_mapper_switch(input_data_s *input)
+{
+
+}
+
+void _mode_mapper_xinput(input_data_s *input)
+{
+
+}
+
+void _mode_mapper_snes(input_data_s *input)
+{
+
+}
+
+void _mode_mapper_n64(input_data_s *input)
+{
+
+}
+
+void _mode_mapper_gamecube(input_data_s *input)
+{
+
+}
+
+void mapper_init()
+{
+    // Clear all digital mapper pointers
+    for(int i = 0; i < MAPPER_CODE_MAX; i++)
+    {
+        _mapper_button_updaters[i] = NULL;
+    }
+}
+
+typedef union 
+{
+    struct 
+    {
+        uint8_t digital_in : 1;
+        uint8_t digital_out : 1;
+        uint8_t joystick_in : 1;
+        uint8_t joystick_out : 1;
+        uint8_t trigger_in : 1;
+        uint8_t trigger_out : 1;
+        uint8_t reserved : 2;
+    };
+    uint8_t value;
+} mapper_remap_type_u;
+
+uint16_t _joystick_thresholds[8] = {1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000};
+uint16_t _trigger_thresholds[2] = {1000, 1000};
+
+void mapper_task()
+{
+    int16_t lx_threshold = 1000;
+    int16_t ly_threshold = 1000;
+    int16_t rx_threshold = 1000;
+    int16_t ry_threshold = 1000;
+    uint16_t lt_threshold = 1000;
+    uint16_t rt_threshold = 1000;
+
+    mapper_input_s tmp;
+
+    for(int i = 0; i < MAPPER_CODE_MAX; i++)
+    {
+        bool down;
+        uint8_t analog_idx_in;
+        uint8_t analog_idx_out;
+
+        // The bitmask of our digital input
+        uint8_t map_digital_bitmask = (1 << i);
+        // The OUTPUT value assigned to the input (i) value
+        int8_t map_code_output = _mapper_profile->map[i];
+        mapper_remap_type_u type = {0};
+
+        switch(i)
+        {
+            default:
+            break;
+
+            case MAPPER_CODE_SOUTH ... MAPPER_CODE_RG_LOWER:
+            type.digital_in = 1;
+            break;
+
+            case MAPPER_CODE_LT_ANALOG ... MAPPER_CODE_RT_ANALOG:
+            type.trigger_in = 1;
+            break;
+
+            case MAPPER_CODE_LX_RIGHT ... MAPPER_CODE_RY_DOWN:
+            type.joystick_in = 1;
+            break;
+        }
+
+        switch(map_code_output)
+        {
+            default:
+            break;
+
+            case MAPPER_CODE_SOUTH ... MAPPER_CODE_RG_LOWER:
+            type.digital_out = 1;
+            break;
+
+            case MAPPER_CODE_LT_ANALOG ... MAPPER_CODE_RT_ANALOG:
+            type.trigger_out = 1;
+            break;
+
+            case MAPPER_CODE_LX_RIGHT ... MAPPER_CODE_RY_DOWN:
+            type.joystick_out = 1;
+            break;
+        }
+
+        switch(type.value)
+        {
+            default:
+            break;
+
+            // Digital -> Digital
+            case 0x03:
+            down = (_mapper_input.digital_inputs & map_digital_bitmask) != 0;
+
+            if(!down) break;
+
+            tmp.digital_inputs |= (1 << map_code_output);
+            break;
+
+            // Digital -> Joystick
+            case 0x09:
+            down = (_mapper_input.digital_inputs & map_digital_bitmask) != 0;
+
+            if(!down) break;
+
+            analog_idx_out = (map_code_output-MAPPER_CODE_IDX_JOYSTICK_START);
+
+            if(_joystick_thresholds[analog_idx_out] > tmp.joysticks_raw[analog_idx_out])
+            {
+                tmp.joysticks_raw[analog_idx_out] = _joystick_thresholds[analog_idx_out];
+            }
+            break;
+
+            // Digital -> Trigger
+            case 0x21:
+            down = (_mapper_input.digital_inputs & map_digital_bitmask) != 0;
+
+            if(!down) break;
+
+            analog_idx_out = (map_code_output-MAPPER_CODE_IDX_TRIGGER_START);
+
+            if(_trigger_thresholds[analog_idx_out] > tmp.triggers[analog_idx_out])
+            {
+                tmp.triggers[analog_idx_out] = _trigger_thresholds[analog_idx_out];
+            }
+            break;
+
+            // Joystick -> Digital
+            case 0x06:
+            analog_idx_in = (i-MAPPER_CODE_IDX_JOYSTICK_START);
+            down = (_mapper_input.joysticks_raw[analog_idx_in] >= _joystick_thresholds[analog_idx_in]);
+
+            if(!down) break;
+
+            tmp.digital_inputs |= (1 << map_code_output);
+            break;
+
+            // Joystick -> Joystick
+            case 0x0C:
+            analog_idx_in = (i-MAPPER_CODE_IDX_JOYSTICK_START);
+            analog_idx_out = (map_code_output-MAPPER_CODE_IDX_JOYSTICK_START);
+
+            if(_mapper_input.joysticks_raw[analog_idx_in] > tmp.joysticks_raw[analog_idx_out])
+            {
+                tmp.joysticks_raw[analog_idx_out] = _mapper_input.joysticks_raw[analog_idx_in];
+            }
+            break;
+
+            // Joystick -> Trigger
+            case 0x24:
+            analog_idx_in = (i-MAPPER_CODE_IDX_JOYSTICK_START);
+            analog_idx_out = (map_code_output-MAPPER_CODE_IDX_TRIGGER_START);
+
+            if(_mapper_input.joysticks_raw[analog_idx_in] > tmp.triggers[analog_idx_out])
+            {
+                tmp.triggers[analog_idx_out] = _mapper_input.joysticks_raw[analog_idx_in];
+            }
+            break;
+
+            // Trigger -> Digital
+            case 0x12:
+            break;
+
+            // Trigger -> Joystick
+            case 0x18:
+            break;
+
+            // Trigger -> Trigger
+            case 0x30:
+            break;
+        }
+    }
+}
+
+// Process all remaps
+void mapper_process()
+{
+    // Clear all digital mapper pointers
+    for(int i = 0; i < MAPPER_CODE_MAX; i++)
+    {
+        bool down = ((_mapper_input.digital_inputs >> i) & 0x1) != 0;
+        if(_mapper_button_updaters[i]) _mapper_button_updaters[i](down);
+    }
+}
